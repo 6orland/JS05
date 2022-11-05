590f5e8a1399f3984e2f2069a09d02046f8a7fba
Your Name <you@example.com>
2022-11-05 13:49:08 +0300

    Условия  if

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log('Привет')
+ const age = 15;
+ const gender = "муж" // жен
+
+if (age>18) {
+    console.log('Вам больше 18 лет')
+}
+
+if (gender === 'муж') {
+    console.log('Вам на лево')
+}
+if (gender === "жен") {
+    console.log('Вам направо')
+}
