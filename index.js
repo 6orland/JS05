2bd7ec1ef563f2c665f4b9f1a276b4da1e6d82d6
Your Name <you@example.com>
2022-11-05 13:53:37 +0300

    Добавили изменение возраста

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
  const age = 15;
- const gender = "муж" // жен
+ const gender = "МУЖ" // жен
 
-if (age>18) {
+if (age > 18) {
     console.log('Вам больше 18 лет')
-}
+} else { 
+    "Вам меньше 18 лет" }
 
 if (gender === 'муж') {
     console.log('Вам на лево')
