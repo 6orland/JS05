763dee36516d1bfa6af376d1c120890f87ed0df9
Your Name <you@example.com>
2022-11-05 14:26:15 +0300

    Проверка возраста

--- index.js
+++ index.js
@@ -1,14 +1,23 @@
- const age = 15;
- const gender = "МУЖ" // жен
+let age;
+// const gender ="муж" // жен
+age = +age;
+if (!isNaN(+age)) {
 
 if (age > 18) {
-    console.log('Вам больше 18 лет')
+    console.log("Вам больше 18 лет");
+} else if (age < 18) {
+    console.log("Вам меньше 18 лет"); 
+} else if(age == 18) {
+    console.log("Вам 18 лет")
+} else {
+    console.log("Error 404")
+}
 } else { 
-    "Вам меньше 18 лет" }
-
-if (gender === 'муж') {
-    console.log('Вам на лево')
+    console.log("Введите корректные данные");
 }
-if (gender === "жен") {
-    console.log('Вам направо')
-}
+// if (gender === "муж"); {
+//     console.log('Вам на лево')
+// }
+// if (gender === "жен"); {
+//     console.log('Вам направо')
+// }
